57cff8b5598e39abeae18d52259a09f5b1e20f9e
samibrahmi <samibrahmi@yahoo.fr>
2016-04-15 11:25:57 +0100

    sript to get all links from a link with time-duration executation

--- first steps/getAll_links.py
+++ first steps/getAll_links.py
@@ -1,7 +1,10 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import time
 html = urlopen("http://www.tayara.tn/")
 bsObj = BeautifulSoup(html)
+startTime = time.time()
 for link in bsObj.findAll("a"):
 	if 'href' in link.attrs:
 		print(link.attrs['href'])
+print (time.time() - startTime)
